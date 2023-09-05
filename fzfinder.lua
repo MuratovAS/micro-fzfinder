33321552b39c10b7ed39502e53cc13456cedf88e
Twix <cdaccounts@protonmail.com>
2023-09-05 12:35:37 +0200

    add fzfcmd option

--- fzfinder.lua
+++ fzfinder.lua
@@ -1,18 +1,22 @@
 VERSION = "0.1.0"
 
-local micro = import("micro")
+local micro  = import("micro")
 local config = import("micro/config")
 local buffer = import("micro/buffer")
-local shell = import("micro/shell")
+local shell  = import("micro/shell")
 
 local fzfarg =  config.GetGlobalOption("fzfarg")
+local fzfcmd =  config.GetGlobalOption("fzfcmd")
 
 function fzfinder(bp)
   if fzfarg == nil then
-    fzfarg = "";
+     fzfarg = "";
+  end
+  if fzfcmd == nil then
+     fzfcmd = "fzf";
   end
 
-  local output, err = shell.RunInteractiveShell("fzf "..fzfarg, false, true)
+  local output, err = shell.RunInteractiveShell("sh -c '"..fzfcmd.." "..fzfarg.."'", false, true)
   if err ~= nil then
     micro.InfoBar():Error(err)
   else
